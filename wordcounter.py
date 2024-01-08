d50068e6a5e76743765838863c9802b1a6b0ee9f
truthseeker300803 <truthseeker300803@gmail.com>
2024-01-08 15:28:57 +0300

    word counter 2

---
+++ wordcounter.py
@@ -0,0 +1,17 @@
+print("Hello!\nThis is a program showing the number of words in a text.")
+num = int(input("How many words do you want to analyze? "))
+fname = input("Enter a file: ")
+fhand = open(fname)
+
+counter = {}
+for line in fhand:
+    words = line.split()
+    for word in words:
+        counter[word] = counter.get(word, 0) + 1
+
+lst = []
+for k,v in counter.items():
+    lst.append((v, k))
+
+print(sorted(lst, reverse=True)[:num])
+
