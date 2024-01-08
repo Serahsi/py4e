a84792f59d0f84be575f8d3b2d7c52b66e7d7b9e
truthseeker300803 <truthseeker300803@gmail.com>
2024-01-08 20:38:57 +0300

    search for a special word added

--- wordcounter.py
+++ wordcounter.py
@@ -1,20 +1,24 @@
 print("Hello!\nThis is a program showing the number of words in a text.")
-num = int(input("How many words do you want to analyze? "))
-fname = input("Enter a file: ")
-fhand = open(fname)
+inp = int(input("What would you like to do?\nFor analyzing whole text press 1\nFor counting a spesific word in the text press 2 "))
 
-counter = {}
+fname = input("Enter a file name: ")
+fhand = open(fname)
+counterd = {}
 for line in fhand:
     words = line.split()
     for word in words:
-        counter[word] = counter.get(word, 0) + 1
+        counterd[word.lower()] = counterd.get(word.lower(), 0) + 1
+
+if inp == 1:
+    num = int(input("How many words do you want to analyze? "))
+    res = sorted([(v,k) for k,v in counterd.items()], reverse=True)[:num]
+    for k,v in res:
+        print("Word <<<", v.upper(), ">>> passes in text for", k, "times")
+elif inp == 2:
+    wrd = input("Enter the word you want to check the count: ").lower()
+    if wrd in counterd.keys():
+        print("Word <<<", wrd.upper(), ">>> passes in text for", counterd[wrd], "times")
+    else:
+        print("Sorry <<<", wrd.upper(), ">>> is NOT in the text")
 
 
-print(sorted([(v,k) for k,v in counter.items()], reverse=True)[:num])
-"""
-lst = []
-for k,v in counter.items():
-    lst.append((v, k))
-
-print(sorted(lst, reverse=True)[:num])
-"""
